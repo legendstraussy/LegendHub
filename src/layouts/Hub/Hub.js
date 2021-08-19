cc431dafe0fc068ed305b1552c51a3222a9221bd
legendstraussy <60454413+legendstraussy@users.noreply.github.com>
2021-08-19 08:39:38 -0500

    adds material ui (#2)

--- src/layouts/Hub/Hub.js
+++ src/layouts/Hub/Hub.js
@@ -1,10 +1,20 @@
 import BackgroundImage from "components/BackgroundImage/BackgroundImage";
 import NavBar from 'components/NavBar/NavBar';
-import styles from './Toolbar.module.css';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  toolbar: {
+    margin: '1em 0 1em 8em',
+    backgroundColor: 'red',
+    display: 'flex',
+  },
+});
 
 const Toolbar = () => {
+  const classes = useStyles();
+
   return (
-    <div className={styles.toolbar}>Toolbar</div>
+    <div className={classes.toolbar}>Toolbar</div>
   )
 }
 
