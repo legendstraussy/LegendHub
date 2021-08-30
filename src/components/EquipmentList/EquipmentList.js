d5a8faef1d0de4bae5a2cc834b697bd6d6f46786
Artizmo <straussy@legendmud.org>
2021-08-29 20:01:11 -0500

    fixes slot row font size

--- src/components/EquipmentList/EquipmentList.js
+++ src/components/EquipmentList/EquipmentList.js
@@ -93,7 +93,7 @@ const useStyles = makeStyles({
       alignItems: 'center',
       cursor: 'default',
       '& [data-field=slot]': {
-        fontSize: '12px',
+        fontSize: '14px',
         fontWeight: '100',
         textTransform: 'capitalize',
         fontStyle: 'italic',
